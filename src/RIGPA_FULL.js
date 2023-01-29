688196ac4b600e2b92aa3c7780ec5531296f2859
Maryna M <markova.maryna@gmail.com>
2023-01-29 18:59:28 +0200

    Add Piece4

--- src/RIGPA_FULL.js
+++ src/RIGPA_FULL.js
@@ -3,6 +3,7 @@ import BuddhaBodies from "./img/BuddhaBodies.jpg";
 import rigpa from "./img/rigpa.png";
 import brain_mirror from "./img/brain_mirror.jpg";
 import mind from "./img/mind.png";
+import mirror from "./img/mirror.png";
 
 export default function RIGPA_FULL() {
   return (
@@ -121,6 +122,62 @@ export default function RIGPA_FULL() {
       </p>
       <div>
         <img className="img-fluid" src={brain_mirror} alt="brain_mirror" />
+      </div>
+      <h3>3</h3>
+      <br />
+      <p>
+        Якщо це блискуче усвідомлення, яке звуть розумом, розглядати як таке, що
+        існує, то воно не існує.{" "}
+      </p>
+      <p>
+        Якщо розглядати як джерело — то самсара й нірвана, й усе розмаїття
+        блаженства й страждання виникли з нього.{" "}
+      </p>
+      <p>
+        Якщо розглядати як об’єкт бажання, то одинадцять колісниць прагнуть до
+        нього.{" "}
+      </p>
+      <br />
+      <p>Імен же йому стільки, що й не злічити.</p>
+      <p>
+        Одні звуть його <em className="term">природою розуму</em> — розум як він
+        є.{" "}
+      </p>
+      <p>
+        Тиртики звуть <em className="term">атман</em> — самість.{" "}
+      </p>
+      <p>
+        Шраваки наполягають на <em className="term">анатмані</em> — відсутності
+        Я.{" "}
+      </p>
+      <p>
+        Читтаматрини звуть його <em className="term">віджняна</em> — свідомість.{" "}
+      </p>
+      <p>
+        Хто кличе <em className="term">Праджняпараміта</em> — Позамежна
+        Мудрість.{" "}
+      </p>
+      <p>
+        Хто кличе <em className="term">Сугатагарбга</em> — насіння Сугати.{" "}
+      </p>
+      <p>
+        Хто кличе <em className="term">Магамудра</em> — Великий Символ.{" "}
+      </p>
+      <p>
+        Хто кличе <em className="term">самотнє Тігле</em> — унікальна сфера.{" "}
+      </p>
+      <p>
+        Хто кличе <em className="term">Дгармадгату</em> — дгармовий простір.{" "}
+      </p>
+      <p>
+        Хто кличе <em className="term">Алая</em> — основа всього.
+      </p>
+      <p>
+        {" "}
+        Хто кличе <em className="term">звичайним усвідомленням</em>.
+      </p>
+      <div>
+        <img className="img-fluid" src={mind} alt="mind" />
       </div>
       <h3>3</h3>
       <br />
