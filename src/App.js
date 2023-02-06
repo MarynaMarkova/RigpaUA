b8cc996e80c749845af1c1eb0c95fe317bcc5de3
Maryna M <markova.maryna@gmail.com>
2023-02-06 20:51:49 +0200

    add Piece27

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import RIGPA_FULL from "./RIGPA_FULL.js";
 import rigpa from "./img/rigpa.png";
-import Piece26 from "./Piece26.js";
+import Piece27 from "./Piece27.js";
 // import RandomNuber from "./RandomNumber";
 import "./App.css";
 
@@ -20,7 +20,7 @@ export default function App() {
         </div>{" "}
         {/* <RIGPA_FULL /> */}
         {/* <RandomNuber /> */}
-        <Piece26 />
+        <Piece27 />
         <div className="Author">
           <p>Падмасамбгава </p>
           <p>«Самовизволення через увагу, яка оголює, – </p>
